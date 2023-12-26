f3fbc55a36018534d142f7e9dc8beb9c120442be
samir-javascript <samirjavascript00@gmail.com>
2023-12-26 18:20:02 +0100

    remove build

--- server/index.js
+++ server/index.js
@@ -43,7 +43,7 @@ if(process.env.NODE_ENV === 'production') {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
    })
 }else {
-  app.get('/', (req, res) => {
+  app.get('/', (req, res) => { 
     res.send('hello world 2024 is coming');
   });
 }
